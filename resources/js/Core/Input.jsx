7d8d1ff0551648ad0fe31b0c39cffa9fdc5277c7
Mohamed Ashour <boxv38145@gmail.com>
2024-06-27 23:27:27 +0300

    Auth page

--- resources/js/Core/Input.jsx
+++ resources/js/Core/Input.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default function Input() {
+export default function Input({placeholder, }) {
     return (
-        <div>Input</div>
+        <input
+            className={`w-full bg-[--theme-body-bg] border-[--theme-nav-bg-color-hover] border-t-transparent rounded hover:border-[--theme-button-border-color] placeholder:text-[--theme-secondary-text-color]`}
+            type="text"
+            placeholder={placeholder}
+        />
     )
 }
--- resources/js/Pages/Auth/Auth.jsx
+++ resources/js/Pages/Auth/Auth.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useApp} from "@/AppContext/AppContext.jsx";
 import {FcGoogle} from "react-icons/fc";
 import {FaFacebook} from "react-icons/fa";
+import Input from "@/Core/Input.jsx";
 
 export default function Auth() {
 
@@ -16,16 +17,13 @@ export default function Auth() {
             />
 
             <div className={`bg-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded`}>
-                <div className={`max-w-[50rem] pb-5 pt-16 text-[--theme-body-color] bg-[--theme-nav-bg-color]`}>
+                <div className={`max-w-[50rem] pt-16 text-[--theme-body-color] bg-[--theme-nav-bg-color]`}>
                     <div className={`flex items-center flex-col pb-6`}>
-                        <div className={`w-fit relative`}>
-                            <img
-                                src={settings.logo}
-                                alt="logo"
-                                className={`w-52`}
-                            />
-                            <span className={`absolute left-0`}>العربية</span>
-                        </div>
+                        <img
+                            src={settings.logo}
+                            alt="logo"
+                            className={`w-52`}
+                        />
                     </div>
 
                     <span className={`font-bold text-[--theme-primary-text-color] text-center block`}>مكان لتبادل المعرفة وفهم العالم بشكل أفضل</span>
@@ -42,7 +40,7 @@ export default function Auth() {
 
 
                             <div className={`mt-6 flex flex-col gap-y-3`}>
-                                <button className={`border border-[--theme-nav-bg-color-hover] flex items-center py-3 gap-x-4 px-4 bg-[--theme-body-bg] hover:bg-[--theme-button-bg-color-hover] transition`}>
+                                <button className={`border border-[--theme-nav-bg-color-hover] flex items-center py-3 gap-x-4 px-4 bg-[--theme-body-bg] hover:bg-[--theme-button-bg-color-hover]`}>
                                     <FaFacebook className={`size-6 text-[--theme-button-border-color]`}/>
                                     <span>المتابعة عن طريق Facebook</span>
                                 </button>
@@ -51,10 +49,37 @@ export default function Auth() {
                             <div className={`text-center mt-5 cursor-pointer font-bold text-sm`}>اشترك عن طريق البريد الإلكتروني</div>
                         </div>
 
-                        <div className={`ps-8`}>
-                            <div className={`border-b border-[--theme-nav-bg-color-hover] w-full pb-2`}>تسجيل الدخول</div>
+                        <div className={`ps-8 flex flex-col gap-y-4`}>
+                            <div className={`border-b border-[--theme-nav-bg-color-hover] w-full pb-2 font-bold`}>تسجيل الدخول</div>
+
+                            <div className={`flex flex-col gap-y-2`}>
+                                <span className={`font-bold`}>البريد الإلكترونى</span>
+                                <Input placeholder={`بريدك الإلكترونى`}/>
+                            </div>
+
+                            <div className={`flex flex-col gap-y-2`}>
+                                <span className={`font-bold`}>كلمة المرور</span>
+                                <Input placeholder={`كلمة المرور الخاصة بك`}/>
+                            </div>
+
+                            <div className={`flex justify-between`}>
+                                <button className={`text-sm text-[--theme-secondary-text-color] hover:underline`}>هل نسيت كلمة المرور؟</button>
+                                <button className={`rounded-full px-4 py-2 bg-[--theme-button-border-color] font-bold`}>تسجيل الدخول</button>
+                            </div>
                         </div>
                     </div>
+
+                    <div className={`w-full bg-[--theme-body-bg] mt-5 py-4 px-8 text-sm text-center`}>
+                        <span className={`hover:underline cursor-pointer`}>حول . </span>
+                        <span className={`hover:underline cursor-pointer`}>الوظائف . </span>
+                        <span className={`hover:underline cursor-pointer`}>الخصوصية . </span>
+                        <span className={`hover:underline cursor-pointer`}>الشروط . </span>
+                        <span className={`hover:underline cursor-pointer`}>الاتصال . </span>
+                        <span className={`hover:underline cursor-pointer`}>اللغات . </span>
+                        <span className={`hover:underline cursor-pointer`}>الصحافة . </span>
+                        <span className={`ms-2`}>© Quora, Inc. 2024</span>
+                    </div>
+
                 </div>
             </div>
         </>
