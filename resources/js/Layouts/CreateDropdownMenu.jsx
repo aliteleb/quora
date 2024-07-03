718fe09b4433fcdb370f7a314c049b187785811c
Mohamed Ashour <boxv38145@gmail.com>
2024-07-03 20:50:52 +0300

    Fix the close animation of Dropdowns

--- resources/js/Layouts/CreateDropdownMenu.jsx
+++ resources/js/Layouts/CreateDropdownMenu.jsx
@@ -1,62 +1,74 @@
-import React, {useEffect, useRef} from 'react'
-import {Transition, TransitionChild} from "@headlessui/react";
-import {BsQuestionSquare} from "react-icons/bs";
-import {BiCommentEdit} from "react-icons/bi";
-import {FaUsers} from "react-icons/fa";
-import {useApp} from "@/AppContext/AppContext.jsx";
+import React, { useEffect, useRef } from 'react';
+import { Transition, TransitionChild } from '@headlessui/react';
+import { BsQuestionSquare } from 'react-icons/bs';
+import { BiCommentEdit } from 'react-icons/bi';
+import { FaUsers } from 'react-icons/fa';
+import { useApp } from '@/AppContext/AppContext.jsx';
 
 export default function CreateDropdownMenu(props) {
-
-    const {setIsCreatThreadModalOpen, setIsPostActive, user } = useApp();
+    const { setIsCreatThreadModalOpen, setIsPostActive, user } = useApp();
 
     const createDropDownRef = useRef(null);
     useEffect(() => {
         const handleClickOutside = (e) => {
-            if (!createDropDownRef.current?.contains(e.target) && e.target.id !== "createDropdown") {
-                props.setIsCreateDropdownMenuOpen(false)
+            if (!createDropDownRef.current?.contains(e.target) && e.target.id !== 'createDropdown') {
+                props.setIsCreateDropdownMenuOpen(false);
             }
-        }
+        };
 
-        window.addEventListener('mousedown', handleClickOutside)
+        window.addEventListener('mousedown', handleClickOutside);
         return () => {
-            window.removeEventListener('mousedown', handleClickOutside)
-        }
+            window.removeEventListener('mousedown', handleClickOutside);
+        };
     }, []);
 
     return (
-        <Transition enter={'duration-500 transition'} show={props.isCreateDropdownMenuOpen} leave="duration-200" ref={createDropDownRef} className={`dropdown-clip-path-responsive 2xl:dropdown-clip-path absolute left-1/2 top-11 2xl:-translate-x-1/2 border border-[--theme-default-border-color] rounded bg-[--theme-main-bg-color] pt-3`}>
-            <TransitionChild
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                as={'div'}
+        <Transition
+            enter="duration-500 transition"
+            show={props.isCreateDropdownMenuOpen}
+            leave="duration-200"
+        >
+            <div
+                ref={createDropDownRef}
+                className={`dropdown-clip-path-responsive 2xl:dropdown-clip-path absolute left-1/2 top-11 2xl:-translate-x-1/2 border border-[--theme-default-border-color] rounded bg-[--theme-main-bg-color] pt-3`}
             >
-                <main>
-                    <div onClick={() => {
-                        setIsCreatThreadModalOpen(true)
-                        setIsPostActive(false)
-                    }} className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60 border-b border-[--theme-default-border-color]`}>
-                        <BsQuestionSquare className={`size-6`}/>
-                        <span>إنشاء سؤال</span>
-                    </div>
-                    <div onClick={() => {
-                        setIsCreatThreadModalOpen(true)
-                        setIsPostActive(true)
-                    }} className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60 border-b border-[--theme-default-border-color]`}>
-                        <BiCommentEdit className={`size-6`}/>
-                        <span>إنشاء منشور</span>
-                    </div>
-                    <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60`}>
-                        <FaUsers className={`size-6`}/>
-                        <span>إنشاء مساحة</span>
-                    </div>
-                </main>
-            </TransitionChild>
-
-
+                <TransitionChild
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    as="div"
+                >
+                    <main>
+                        <div
+                            onClick={() => {
+                                setIsCreatThreadModalOpen(true);
+                                setIsPostActive(false);
+                            }}
+                            className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60 border-b border-[--theme-default-border-color]`}
+                        >
+                            <BsQuestionSquare className={`size-6`} />
+                            <span>إنشاء سؤال</span>
+                        </div>
+                        <div
+                            onClick={() => {
+                                setIsCreatThreadModalOpen(true);
+                                setIsPostActive(true);
+                            }}
+                            className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60 border-b border-[--theme-default-border-color]`}
+                        >
+                            <BiCommentEdit className={`size-6`} />
+                            <span>إنشاء منشور</span>
+                        </div>
+                        <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3 w-60`}>
+                            <FaUsers className={`size-6`} />
+                            <span>إنشاء مساحة</span>
+                        </div>
+                    </main>
+                </TransitionChild>
+            </div>
         </Transition>
-    )
+    );
 }
--- resources/js/Layouts/UserDropdownMenu.jsx
+++ resources/js/Layouts/UserDropdownMenu.jsx
@@ -31,50 +31,49 @@ export default function UserDropdownMenu({isUserDropdownMenuOpen ,setIsUserDropd
     }
 
     return (
-        <Transition enter={'duration-500 transition'} show={isUserDropdownMenuOpen} leave="duration-200" ref={dropDownRef} className={`dropdown-clip-path-responsive md:dropdown-clip-path absolute left-1/2 top-11 md:-translate-x-1/2 border border-[--theme-default-border-color] rounded bg-[--theme-main-bg-color] w-64 2xl:w-fit pt-8 py-3`}>
-            <TransitionChild
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                as={'div'}
-            >
-                <header>
-                    <div className={`flex items-center gap-x-36 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-2 px-3`}>
-                        <div className={`flex items-center gap-x-4`}>
-                            {user?.avatar && <img onClick={() => setIsUserDropdownMenuOpen(true)} src={``} className={`md:size-9 size-7 rounded-full cursor-pointer `}/>}
-                            {(!user?.avatar && user) && <FaRegCircleUser onClick={() => setIsUserDropdownMenuOpen(true)} className={`md:size-9 size-7 cursor-pointer text-[--theme-placeholder-color]`}/>}
-                            <span className={`font-bold`}>{user?.name}</span>
+        <Transition show={isUserDropdownMenuOpen} leave="duration-200 transition-all">
+            <div ref={dropDownRef} className={`dropdown-clip-path-responsive md:dropdown-clip-path absolute left-1/2 top-11 md:-translate-x-1/2 border border-[--theme-default-border-color] rounded bg-[--theme-main-bg-color] w-64 2xl:w-fit pt-8 py-3`}>
+                <TransitionChild
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    as={'div'}
+                >
+                    <header>
+                        <div className={`flex items-center gap-x-36 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-2 px-3`}>
+                            <div className={`flex items-center gap-x-4`}>
+                                {user?.avatar && <img onClick={() => setIsUserDropdownMenuOpen(true)} src={user.avatar} className={`md:size-9 size-7 rounded-full cursor-pointer`}/>}
+                                {(!user?.avatar && user) && <FaRegCircleUser onClick={() => setIsUserDropdownMenuOpen(true)} className={`md:size-9 size-7 cursor-pointer text-[--theme-placeholder-color]`}/>}
+                                <span className={`font-bold`}>{user?.name}</span>
+                            </div>
+                            <BsChevronLeft className={`size-6`}/>
                         </div>
-                        <BsChevronLeft  className={`size-6`}/>
-                    </div>
-                    <div className={`bg-[--theme-default-border-color] h-[1px] w-full mt-3`}></div>
-                </header>
-                <main className={`py-4 pb-4 border-b border-[--theme-default-border-color] 2xl:text-sm text-[15px]`}>
-                    <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
-                        <BsBarChart className={`size-6`}/>
-                        <span>المحتوي والإحصاءات الخاصة بك</span>
-                    </div>
-                    <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
-                        <FaRegBookmark className={`size-6`}/>
-                        <span>إشارات مرجعية</span>
-                    </div>
-                    <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
-                        <FaUsers className={`size-6`}/>
-                        <span>إنشاء مساحة</span>
-                    </div>
-                </main>
-                <footer className={`pt-3 px-2 flex flex-row-reverse`}>
-                    <button onClick={logout} className={`bg-[--theme-primary-button-color] hover:bg-opacity-90 px-4 py-1.5 rounded-3xl`}>
-                        تسجيل خروج
-                    </button>
-                </footer>
-            </TransitionChild>
-
-
+                        <div className={`bg-[--theme-default-border-color] h-[1px] w-full mt-3`}></div>
+                    </header>
+                    <main className={`py-4 pb-4 border-b border-[--theme-default-border-color] 2xl:text-sm text-[15px]`}>
+                        <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
+                            <BsBarChart className={`size-6`}/>
+                            <span>المحتوي والإحصاءات الخاصة بك</span>
+                        </div>
+                        <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
+                            <FaRegBookmark className={`size-6`}/>
+                            <span>إشارات مرجعية</span>
+                        </div>
+                        <div className={`flex items-center gap-x-5 hover:bg-[--theme-nav-bg-color-hover] cursor-pointer py-3 px-3`}>
+                            <FaUsers className={`size-6`}/>
+                            <span>إنشاء مساحة</span>
+                        </div>
+                    </main>
+                    <footer className={`pt-3 px-2 flex flex-row-reverse`}>
+                        <button onClick={logout} className={`bg-[--theme-primary-button-color] hover:bg-opacity-90 px-4 py-1.5 rounded-3xl`}>
+                            تسجيل خروج
+                        </button>
+                    </footer>
+                </TransitionChild>
+            </div>
         </Transition>
-
     )
 }
