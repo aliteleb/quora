5f43fc8e85f7335d9438869892830eb2f7514307
aliteleb <aliteleb123456@gmail.com>
2024-07-04 00:23:30 +0300

    Just a commit

--- resources/js/Pages/Home/Partials/Footer.jsx
+++ resources/js/Pages/Home/Partials/Footer.jsx
@@ -7,35 +7,31 @@ export default function Footer() {
     const {setIsSpaceModalOpen} = useApp()
 
     return (
-            <div className={`top-16 fixed text-[--theme-primary-text-color] gap-y-7 sm:flex hidden flex-col`}>
-                <button onClick={() => {
-                    setIsSpaceModalOpen(true)
-                }} className={`w-fit flex gap-x-3 items-center bg-[#1b1b1b] hover:bg-[#1d1d1d] px-6 py-2 rounded transition`}>
-                        <LuPlus className={`bg-[#262626] p-1 rounded text-xl`}/>
-                        <span>إنشاء مساحة</span>
-                </button>
+        <div className={`top-16 fixed text-[--theme-primary-text-color] gap-y-7 sm:flex hidden flex-col`}>
+            <button onClick={() => {
+                setIsSpaceModalOpen(true)
+            }} className={`w-fit flex gap-x-3 items-center bg-[#1b1b1b] hover:bg-[#1d1d1d] px-6 py-2 rounded transition`}>
+                <LuPlus className={`bg-[#262626] p-1 rounded text-xl`}/>
+                <span>إنشاء مساحة</span>
+            </button>
 
-                <div className={`text-[--theme-secondary-text-color]`}>
-
-                    <div>
-                        <a href="" className={`hover:underline`}>حول . </a>
-                        <a href="" className={`hover:underline`}>الوظائف .</a>
-                    </div>
-                    <div>
-                        <a href="" className={`hover:underline`}>الشروط . </a>
-                        <a href="" className={`hover:underline`}>الخصوصية .</a>
-                    </div>
-                    <div>
-                        <a href="" className={`hover:underline`}>الاستخدام المقبول .</a>
-                    </div>
-                    <div>
-                        <a href="" className={`hover:underline`}>إعلان . </a>
-                        <a href="" className={`hover:underline`}>الصحافة</a>
-                    </div>
-
+            <div className={`text-[--theme-secondary-text-color]`}>
+                <div>
+                    <a href="" className={`hover:underline`}>حول . </a>
+                    <a href="" className={`hover:underline`}>الوظائف .</a>
                 </div>
-
+                <div>
+                    <a href="" className={`hover:underline`}>الشروط . </a>
+                    <a href="" className={`hover:underline`}>الخصوصية .</a>
+                </div>
+                <div>
+                    <a href="" className={`hover:underline`}>الاستخدام المقبول .</a>
+                </div>
+                <div>
+                    <a href="" className={`hover:underline`}>إعلان . </a>
+                    <a href="" className={`hover:underline`}>الصحافة</a>
+                </div>
+            </div>
         </div>
-
     )
 }
