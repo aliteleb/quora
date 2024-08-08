00b464d7e65d76fd5a260051c5ec83cf83087fc0
Mohamed Ashour <boxv38145@gmail.com>
2024-08-08 19:21:10 +0300

    Profile page => Follow user

--- resources/js/Pages/Profile/Layouts/Header.jsx
+++ resources/js/Pages/Profile/Layouts/Header.jsx
@@ -8,8 +8,10 @@ import {followUser} from "@/Utilities/followUser.js";
 
 export default function Header({isActive, setIsActive, profileUserInfo}) {
     const {props} = usePage()
-    const [user, setUser] = useState(props?.user?.data);
-    const [isUserFollowed, setIsUserFollowed] = useState(false);
+    const { user } = useApp()
+    const [userInfo, setUserInfo] = useState(props?.user?.data);
+    const [isFollowed, setIsFollowed] = useState(true);
+
 
     const handleClickOnButton = (e) => {
         const button = e.target.getAttribute('select')
@@ -47,25 +49,29 @@ export default function Header({isActive, setIsActive, profileUserInfo}) {
                 />
                 <div className={`flex flex-col gap-y-2 w-full`}>
                     <div className={`flex justify-between`}>
-                        <h1 className={`text-3xl font-bold`}>{user?.name}</h1>
-                        <Button content={`تعديل`} custom_styles={`p-1 bg-transparent hover:bg-[--theme-main-bg-color] transition rounded-full px-4 py-2`}/>
+                        <h1 className={`text-3xl font-bold`}>{userInfo?.name}</h1>
+                        {userInfo.id === user.id &&
+                            <Button content={`تعديل`} custom_styles={`p-1 bg-transparent hover:bg-[--theme-main-bg-color] transition rounded-full px-4 py-2`}/>
+                        }
                     </div>
-                    <div className={`${!user?.bio ? 'text-[--theme-placeholder-color]' : ''}`}>{user?.bio ? user.bio : 'نبذة'}</div>
+                    <div className={`${!userInfo?.bio ? 'text-[--theme-placeholder-color]' : ''}`}>{userInfo?.bio ? userInfo.bio : 'نبذة'}</div>
                     <div className={`flex justify-between `}>
                         <div>
                             <span>0 متابعين · </span>
                             <span> يتابع 0</span>
                         </div>
-                        <div className={`flex gap-x-2`}>
-                            <Button
-                                onClick={() => followUser(profileUserInfo.id, setIsUserFollowed)}
-                                content={isUserFollowed ? 'تمت المتابعة' : 'متابعة'}
-                                custom_styles={`border ${!isUserFollowed ? 'border-transparent' : 'bg-transparent'} `}
-                                isTypeFollow={true}
-                                isFollowed={isUserFollowed}
-                            />
-                            <Button content={`حظر`} custom_styles={`bg-[--theme-primary-button-color]`}/>
-                        </div>
+                        {userInfo?.id !== user?.id &&
+                            <div className={`flex gap-x-2`}>
+                                <Button
+                                    onClick={() => followUser(profileUserInfo.id, setIsFollowed, isFollowed)}
+                                    content={isFollowed ? 'تمت المتابعة' : 'متابعة'}
+                                    custom_styles={`border ${!isFollowed ? 'border-transparent' : 'bg-transparent'} `}
+                                    isTypeFollow={true}
+                                    isFollowed={isFollowed}
+                                />
+                                <Button content={`حظر`} custom_styles={`bg-[--theme-primary-button-color]`}/>
+                            </div>
+                        }
                     </div>
 
                 </div>
@@ -74,9 +80,9 @@ export default function Header({isActive, setIsActive, profileUserInfo}) {
             <div className={`flex flex-col gap-y-3`}>
                 <div className={`mt-5 flex border-b border-[--theme-secondary-bg-color-hover]`}>
                     <ProfileButton select={`profile`} custom_styles={isActive.profile ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`الملف الشخصي`}/>
-                    <ProfileButton select={`answers`} custom_styles={isActive.answers ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${user?.answers_count} إجابات`}/>
-                    <ProfileButton select={`questions`} custom_styles={isActive.questions ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${user?.questions_count} أسئلة`}/>
-                    <ProfileButton select={`posts`} custom_styles={isActive.posts ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${user?.posts_count} منشور`}/>
+                    <ProfileButton select={`answers`} custom_styles={isActive.answers ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${userInfo?.answers_count} إجابات`}/>
+                    <ProfileButton select={`questions`} custom_styles={isActive.questions ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${userInfo?.questions_count} أسئلة`}/>
+                    <ProfileButton select={`posts`} custom_styles={isActive.posts ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`${userInfo?.posts_count} منشور`}/>
                     <ProfileButton select={`followers`} custom_styles={isActive.followers ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`0 متابعين`}/>
                     <ProfileButton select={`following`} custom_styles={isActive.following ? 'border-[--theme-primary-button-color] text-[--theme-primary-button-color]' : 'border-transparent'} onClick={handleClickOnButton} content={`يتابع`}/>
                 </div>
